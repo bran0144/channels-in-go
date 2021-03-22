72a801c30db98a3f4fc68d5ddb7814871785d8fb
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-22 14:44:00 -0500

    directional channels

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
+		time.Sleep(5 * time.Millisecond)
 		wg.Done()
 	}(ch, wg)
 
