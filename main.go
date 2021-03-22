da979b7702585241c2f529e5957175030cdc48f4
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-22 14:59:27 -0500

    using for loops

--- main.go
+++ main.go
@@ -8,16 +8,17 @@ import (
 func main() {
 	wg := &sync.WaitGroup{}
 	ch := make(chan int, 1)
-
 	wg.Add(2)
 	go func(ch <-chan int, wg *sync.WaitGroup) {
-		if msg, ok := <-ch; ok {
-			fmt.Println(msg, ok)
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
 		}
 		wg.Done()
 	}(ch, wg)
 	go func(ch chan<- int, wg *sync.WaitGroup) {
-		close(ch)
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
 		wg.Done()
 	}(ch, wg)
 
